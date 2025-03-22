311b30c65d3bf5de85bed38d06529f1e81144080
Joaquim Silva <1241889@isep.ipp.pt>
2025-03-22 01:59:45 +0000

    Nova estrutura

---
+++ src/test/java/com/lp2/inceptus/InceptusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp2.inceptus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InceptusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
