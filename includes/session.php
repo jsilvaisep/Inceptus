9de091b286f1a81859030ce82531af21f9d8f29d
“PeterOliveira” <1241622@isep.ipp.pt>
2025-03-26 17:38:05 +0000

    session - funcional

--- includes/session.php
+++ includes/session.php
@@ -1,10 +1,15 @@
 <?php
+session_save_path(__DIR__ . '/sessions');
+
+if (!file_exists(session_save_path())) {
+    mkdir(session_save_path(), 0777, true);
+}
+
 session_start();
 
 function isLoggedIn() {
     return isset($_SESSION['user']);
 }
-
 
 if (!isset($_SESSION['user_id'])) {
     header("Location: /auth/login.php");
