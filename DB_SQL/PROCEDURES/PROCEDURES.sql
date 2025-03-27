4b7e93ee62ad3d6ecacc2dea5e77a4f49ffad2fb
Rui Maia <1232327@isep.ipp.pt>
2025-03-27 21:47:30 +0000

    Update SQL files

--- DB_SQL/PROCEDURES/PROCEDURES.sql
+++ DB_SQL/PROCEDURES/PROCEDURES.sql
@@ -1,3 +1,31 @@
+/* 
+####################################
+####### CALC AVG #######################################################################################################################################################################
+####################################
+*/
+
+-- RANK DA EMPRESA
+SELECT COMPANY_RANK FROM COMPANY WHERE COMPANY_ID = '${COMPANY_ID}';
+
+UPDATE COMPANY 
+SET COMPANY_RANK = (SELECT AVG(COMMENT_RANK) FROM COMMENT WHERE COMPANY_ID = '${COMPANY_ID}')
+WHERE COMPANY_ID = '${COMPANY_ID}'
+;
+
+-- RANK DO PRODUTO
+SELECT PRODUCT_RANK FROM PRODUCT WHERE PRODUCT_ID = '${PRODUCT_ID}';
+
+UPDATE PRODUCT 
+SET PRODUCT_RANK = (SELECT AVG(COMMENT_RANK) FROM COMMENT WHERE PRODUCT_ID = '${PRODUCT_ID}')
+WHERE PRODUCT_ID = '${PRODUCT_ID}'
+;
+
+/* 
+####################################
+####### PIN RESET PASSWORD #######################################################################################################################################################################
+####################################
+*/
+
 CREATE PROCEDURE CREATE_RESET_CODE (IN V_USER_ID INT)
 BEGIN
 	INSERT INTO DB_INCEPTUS_PP.SEC_RST_CODE
@@ -5,21 +33,32 @@ BEGIN
 	VALUES(V_USER_ID,FLOOR(100000 + RAND() * 900000));
 END
 
-Create Procedure COMPANY_TOP() 
-BEGIN
-	select * from COMPANY c order by c.COMPANY_RANK desc limit 3;
-END
+/* 
+####################################
+####### TOP BY RANK #######################################################################################################################################################################
+####################################
+*/
 
+CREATE PROCEDURE COMPANY_TOP() 
+BEGIN
+	SELECT * FROM COMPANY C ORDER BY C.COMPANY_RANK DESC LIMIT 3;
+END
 
 CREATE PROCEDURE PRODUCT_TOP()
 BEGIN
-select p.PRODUCT_NAME, p.PRODUCT_DESCRIPTION, p.IMG_URL  from PRODUCT p ORDER BY p.PRODUCT_RANK DESC LIMIT 3;
+SELECT P.PRODUCT_NAME, P.PRODUCT_DESCRIPTION, P.IMG_URL  FROM PRODUCT P ORDER BY P.PRODUCT_RANK DESC LIMIT 3;
 END
+
+/* 
+####################################
+####### EMPRESAS MAIS RECENTES #######################################################################################################################################################################
+####################################
+*/
 
 CREATE PROCEDURE TRENDING_TOP()
 BEGIN
-	SELECT c.COMMENT_TEXT, com.COMPANY_NAME 
-	FROM COMMENT c
-	INNER JOIN COMPANY com ON c.COMPANY_ID = com.COMPANY_ID
-	ORDER BY c.CREATED_AT DESC LIMIT 3;
+	SELECT C.COMMENT_TEXT, COM.COMPANY_NAME 
+	FROM COMMENT C
+	INNER JOIN COMPANY COM ON C.COMPANY_ID = COM.COMPANY_ID
+	ORDER BY C.CREATED_AT DESC LIMIT 3;
 END
