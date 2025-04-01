87cdbb23415f7362dac9859d91bc73cb12d26a6d
Rui Maia <1232327@isep.ipp.pt>
2025-04-01 15:53:25 +0100

    update SQL files

--- DB_SQL/INSERT_DATA/UDA.sql
+++ DB_SQL/INSERT_DATA/UDA.sql
@@ -1,14 +1,32 @@
 select * from UDA;
 DELETE from UDA ;
 
-INSERT INTO UDA (UDA_ID, PRODUCT_ID, UDA_VALUES)
+INSERT INTO UDA (UDA_ID, UDA_VALUES)
 VALUES
-(UUID(), 'fefecd40-0bf4-11f0-b0d3-020017000d59', '{
-"motor" : {"potencia": "500w", "rpm": "20"}, 
+(UUID(), '{
+"CATEGORIA" : "DRONES",
+"motor" : 
+	{"potencia": "500w",
+	"rpm": "20"}
+}');
+
+INSERT INTO UDA (UDA_ID, UDA_VALUES)
+VALUES
+(UUID(), '{
+"CATEGORIA" : "SOFTWARE",
+"frontend" : "Angular" , 
+"backend" : ".NET" 
+}');
+
+INSERT INTO UDA (UDA_ID, UDA_VALUES)
+VALUES
+(UUID(), '{
+"CATEGORIA" : "MICRO-ONDAS",
 "grill" : {"potencia": "200w"}, 
 "micro-ondas" : {"pot_min" : "200w", "pot_max" : "3000w"}
 }');
 
-SELECT u.UDA_VALUES  FROM UDA u ;
-select u.UDA_VALUES->>'$.motor.potencia' from UDA u 
+SELECT * FROM UDA u where u.UDA_VALUES->>'$.CATEGORIA' = 'DRONES';
+
+select u.UDA_VALUES->>'$.motor' from UDA u where u.UDA_VALUES->>'$.CATEGORIA' = 'DRONES';  
 ;
